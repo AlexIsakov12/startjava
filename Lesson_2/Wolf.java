183cd0760157e30cfcfa0ea07d7a25339c9f0f0c
AlexIsakov12 <alexisakov2912@gmail.com>
2021-10-12 01:07:58 +0400

    added new classes

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,24 @@
+public class Wolf {
+	String sex = "male";
+	String name = "Chief";
+	String color = "gray";
+
+	int weight = 50;
+	int age = 10;
+
+	void walk() {
+		return "walking";
+	}
+
+	void sit() {
+		return "sitting";
+	}
+
+	void howl() {
+		return "howling";
+	}
+
+	boolean isHunting() {
+		return true;
+	}
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,11 @@
+public class WolfTest {
+	public static void main(String[] args) {
+		Wolf newWolf = new Wolf();
+
+		System.out.println(newWolf.name);
+		System.out.println(newWolf.color);
+
+		newWolf.walk;
+		newWolf.isHunting;
+	}
+}
