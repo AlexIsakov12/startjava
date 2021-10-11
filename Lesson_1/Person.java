cc63c8aaa01c17c982ca9a021fb302e53768949d
AlexIsakov12 <alexisakov2912@gmail.com>
2021-10-12 00:22:20 +0400

    made Person class

---
+++ Lesson_1/Person.java
@@ -0,0 +1,25 @@
+public class Person { 
+	String sex = "male";
+	String name = "James";
+
+	int height = 180;
+	int age = 20;
+
+	float weight = 75.6f;
+
+	void walk() {
+		System.out.println("walk around");
+	}
+
+	void sit() {
+
+	}
+
+	void speak() {
+		System.out.println("saying something");
+	}
+
+	String learnJava() {
+		return "learning";
+	}
+}
