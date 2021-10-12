fb77110aca1c0b5e0632ec5257ca2a3becbcbfb7
AlexIsakov12 <alexisakov2912@gmail.com>
2021-10-12 19:56:45 +0400

    new features

--- Lesson_2/GuessTheNumberTest.java
+++ Lesson_2/GuessTheNumberTest.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 public class GuessTheNumberTest {
 
 	public static void main(String[] args) {
+
+		String restart;
+
+		Scanner console = new Scanner(System.in);
+
 		GuessTheNumber game = new GuessTheNumber();
 		game.Start();
-	}
+
+		System.out.println("Do you want to start a new game? [yes/no] ");
+		restart = console.next();
+
+		while (restart.equals("yes")) {
+			game.Start();
+			System.out.println("Do you want to start a new game? [yes/no] ");
+			restart = console.next();
+		}
+	}	
 }
